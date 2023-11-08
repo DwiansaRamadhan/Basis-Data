2604c40668f5f698ea76405657ec823392720b09
DwiansaRamadhan <128897320+DwiansaRamadhan@users.noreply.github.com>
2023-11-08 20:25:49 +0700

    Add files via upload

---
+++ Jawaban1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mahasiswa(
+				nim VARCHAR(50),
+				nama_mahasiswa VARCHAR(255),
+				jurusan VARCHAR(255)
+);
+
+CREATE TABLE dosen(
+				nama_dosen VARCHAR(255),
+				kode_dosen VARCHAR(50),
+				mata_kuliah VARCHAR(255)
+);
+
+CREATE TABLE mata_kuliah(
+				ilmu_komputer VARCHAR(255)
+);
